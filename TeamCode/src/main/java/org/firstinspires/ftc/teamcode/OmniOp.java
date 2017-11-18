c0d5e1acfa9cc0fd1cc245f82bb783d77f46ff6e
marco091 <marco091@users.noreply.github.com>
2017-11-17 17:46:27 -0800

    Add files via upload

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniOp.java
@@ -0,0 +1,137 @@
+package org.firstinspires.ftc.teamcode;
+
+
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.teamcode.Vector;
+
+/**
+
+ * Created by Sage Creek Level Up on 11/8/2017.
+
+ */
+
+@TeleOp(name = "Omni Op", group = "TeleOp")
+
+public class OmniOp extends OpMode {
+    private DcMotor motora;
+    private DcMotor motorb;
+    private DcMotor motorc;
+    private DcMotor motord;
+
+    @Override
+    public void init() {
+
+        motora = hardwareMap.dcMotor.get("motor1");
+        motorb = hardwareMap.dcMotor.get("motor2");
+        motorc = hardwareMap.dcMotor.get("motor4");
+        motord = hardwareMap.dcMotor.get("motor3");
+
+        motora.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorb.setDirection(DcMotorSimple.Direction.FORWARD);
+        motorc.setDirection(DcMotorSimple.Direction.FORWARD);
+        motord.setDirection(DcMotorSimple.Direction.REVERSE);
+
+    }
+
+    @Override
+    public void init_loop() {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void loop() {
+        Vector inputVector = new Vector(gamepad1.left_stick_x,-gamepad1.left_stick_y); //gamepads are weird, -1 is at the top of the y axis
+        Vector motionVector = inputVector.rotate(-Math.PI/4);
+
+        if(gamepad1.left_bumper && motionVector.isZeroVector()) {
+            motora.setPower(-1);
+            motorb.setPower(1);
+            motorc.setPower(1);
+            motord.setPower(-1);
+        }
+
+        else if(gamepad1.right_bumper && motionVector.isZeroVector()) {
+            motora.setPower(1);
+            motorb.setPower(-1);
+            motorc.setPower(-1);
+            motord.setPower(1);
+        }
+
+        else {
+
+            double offset = (gamepad1.left_trigger-gamepad1.right_trigger)/4;
+
+            motora.setPower(Range.clip(motionVector.x - offset,-1,1));
+            motorb.setPower(Range.clip(motionVector.y + offset,-1,1));
+            motorc.setPower(Range.clip(motionVector.x + offset,-1,1));
+            motord.setPower(Range.clip(motionVector.y - offset,-1,1));
+        }
+    }
+}
+
+/*
+    strafe right
+        motora.setPower(1);
+        motorb.setPower(-1);
+        motorc.setPower(-1);
+        motord.setPower(1);
+
+
+
+    strafe left
+        motora.setPower(-1);
+        motorb.setPower(1);
+        motorc.setPower(1);
+        motord.setPower(-1);
+
+
+    spin left
+        motora.setPower(-1);
+        motorb.setPower(1);
+        motorc.setPower(-1);
+        motord.setPower(1);
+
+        motora.setPower(-motionVector.x);
+        motorb.setPower(motionVector.y);
+        motorc.setPower(-motionVector.y);
+        motord.setPower(motionVector.x);
+
+    spin right
+        motora.setPower(1);
+        motorb.setPower(-1);
+        motorc.setPower(1);
+        motord.setPower(-1);
+
+        motora.setPower(-motionVector.x);
+        motorb.setPower(motionVector.y);
+        motorc.setPower(-motionVector.y);
+        motord.setPower(motionVector.x);
+
+    move
+        motora.setPower(1);
+        motorb.setPower(1);
+        motorc.setPower(1);
+        motord.setPower(1);
+
+        motora.setPower(-motionVector.x);
+        motorb.setPower(motionVector.y);
+        motorc.setPower(-motionVector.y);
+        motord.setPower(motionVector.x);
+
+    spin-move
+    strate-canceled
+ */
